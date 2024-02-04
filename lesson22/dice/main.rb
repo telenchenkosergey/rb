df80d901bc3ec75013ee2c87acc29ef7a1ffd313
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-02-04 17:33:50 +0300

    Add multiple dice and sum

--- lesson22/dice/main.rb
+++ lesson22/dice/main.rb
@@ -8,5 +8,15 @@ def roll
   end
 end
 
-roll
-puts rand(6) + 1
+puts 'How many dice?'
+amount = $stdin.gets.to_i
+
+sum = 0
+
+amount.times do
+  roll
+  dice = rand(6) + 1
+  sum += dice
+  puts dice
+end
+puts "Sum of dice: #{sum}"
