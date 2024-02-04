2703f668f97f1aab0710052055d730db705c3ad7
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-02-04 18:12:49 +0300

    Add Unicode characters

--- lesson23/pick_a_card/main.rb
+++ lesson23/pick_a_card/main.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal
 
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-suits = %w[Spades Diamonds Clubs Hearts]
+suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
 deck = []
 
 values.each do |value|
-  suits.each { |suit| deck << "#{value} of #{suit}" }
+  suits.each { |suit| deck << "#{value} #{suit.encode('utf-8')}" }
 end
 
 puts deck.sample
