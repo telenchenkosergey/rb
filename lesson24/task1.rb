852418267c8fbba35f677306bf7f219f2aefd969
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-02-04 19:34:36 +0300

    Add HW24 Task1 & Task2

---
+++ lesson24/task1.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+puts "В числе 42 - #{42.size} байт"
+puts "В строке 'Василий' - #{'Василий'.bytesize} байт"
---
+++ lesson24/task2.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+a = nil
+
+print "Переменная 'a' "
+puts defined?(a) ? 'определена' : 'не определена'
+
+print "Переменная 'b' "
+puts defined?(b) ? 'определена' : 'не определена'
