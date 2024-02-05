c92e36802e9f198b1ca28956232a8935827f4e11
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-02-05 15:36:52 +0300

    Modify Passenger

--- lesson25/passenger.rb
+++ lesson25/passenger.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-ticket = {
+ticket1 = {
   :number => 'PM2010398 050298',
   :destination => 'Самара',
   :departure => 'Луганск',
@@ -10,7 +10,36 @@ ticket = {
   :passport => 'HB1234567'
 }
 
-puts "Билет №: #{ticket[:number]}"
-puts "Маршрут: #{ticket[:departure]} - #{ticket[:destination]}"
-puts "Пассажир: #{ticket[:first_name]} #{ticket[:middle_name][0]}. #{ticket[:last_name]}"
-puts "Паспорт: #{ticket[:passport]}"
+ticket2 = {
+  :number => 'PM2083798 056588',
+  :destination => 'Анапа',
+  :departure => 'Ростов-на-Дону',
+  :first_name => 'Анатолий',
+  :middle_name => 'Михайлович',
+  :last_name => 'Шпак',
+  :passport => 'HB0954567'
+}
+
+ticket3 = {
+  :number => 'PM1110398 050298',
+  :destination => 'Саратов',
+  :departure => 'Тольятти',
+  :first_name => 'Антонина',
+  :middle_name => 'Лаврентьевна',
+  :last_name => 'Хачатурян',
+  :passport => 'МР1265748'
+}
+
+tickets = [
+  ticket1,
+  ticket2,
+  ticket3
+]
+
+tickets.each_with_index do |ticket, i|
+  puts "* * * Место № #{i + 1} * * *"
+  puts "Билет №: #{ticket[:number]}"
+  puts "Маршрут: #{ticket[:departure]} - #{ticket[:destination]}"
+  puts "Пассажир: #{ticket[:first_name]} #{ticket[:middle_name][0]}. #{ticket[:last_name]}"
+  puts "Паспорт: #{ticket[:passport]}"
+end
