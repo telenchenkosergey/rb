b3abd331104153f57b19d443d3bf58388c02d9f0
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-02-15 20:37:08 +0300

    Add memo read from console method

--- lesson27/notepad/lib/memo.rb
+++ lesson27/notepad/lib/memo.rb
@@ -3,6 +3,14 @@
 # Memo class
 class Memo < Post
   def read_from_console
+    puts 'Введите вашу запись (q - конец ввода)'
+    @text = []
+    line = nil
+    while line != 'q'
+      line = $stdin.gets.chomp
+      @text << line
+    end
+    @text.pop
   end
 
   def to_strings
