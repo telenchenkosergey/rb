75f7afb31c38e26a70aa280d80cbb78f74661e90
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-02-15 20:59:47 +0300

    Finish Notepad App

--- lesson27/notepad/main.rb
+++ lesson27/notepad/main.rb
@@ -5,4 +5,19 @@ require_relative './lib/link'
 require_relative './lib/memo'
 require_relative './lib/task'
 
+puts 'Привет! Я твой Блокнот.'
+puts 'Что хотите записать?'
 
+choices = Post.post_types
+choice = -1
+
+until choice >= 0 && choice <= choices.size
+  choices.each_with_index { |type, index| puts "#{index}. #{type}" }
+  choice = $stdin.gets.to_i
+end
+
+entry = Post.create(choice)
+entry.read_from_console
+entry.save
+
+puts 'Запись сохранена'
