8af29be18ff532968128d95dee18a2de8ead7c46
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-02-15 20:52:28 +0300

    Add static methods to Post class

--- lesson27/notepad/lib/post.rb
+++ lesson27/notepad/lib/post.rb
@@ -2,6 +2,13 @@
 
 # Post class
 class Post
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    post_types[type_index].new
+  end
   def initialize
     @created_at = Time.now
     @text = nil
---
+++ lesson27/notepad/main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative './lib/post'
+require_relative './lib/link'
+require_relative './lib/memo'
+require_relative './lib/task'
+
+
