540e27aec2b949206ab2e6f5eaecf6258f39d95c
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-02-15 20:42:36 +0300

    Add task and link read from console methods

--- lesson27/notepad/lib/link.rb
+++ lesson27/notepad/lib/link.rb
@@ -8,6 +8,10 @@ class Link < Post
   end
 
   def read_from_console
+    puts 'Введите адрес ссылки:'
+    @url = $stdin.gets.chomp
+    puts 'Что это за ссылка?'
+    @text = $stdin.gets.chomp
   end
 
   def to_strings
--- lesson27/notepad/lib/task.rb
+++ lesson27/notepad/lib/task.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'date'
+
 # Task class
 class Task < Post
   def initialize
@@ -8,6 +10,11 @@ class Task < Post
   end
 
   def read_from_console
+    puts 'Что нужно сделать?'
+    @text = $stdin.gets.chomp
+    puts 'К какому числу нужно сделать? (ДД.ММ.ГГГГ)'
+    input = $stdin.gets.chomp
+    @due_date = Date.parse(input)
   end
 
   def to_strings
