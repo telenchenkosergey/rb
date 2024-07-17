24ca1cd1a817b615954892b05060dc1a755fa911
Sergey Telenchenko <51914524+telenchenkosergey@users.noreply.github.com>
2024-07-17 20:22:03 +0300

    Update main.rb

--- lesson09/main.rb
+++ lesson09/main.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 def declension(amount, one, few, many)
+  if amount % 100 >= 11 && amount % 100 <= 14
+    return many
+  end
   if amount % 10 == 1 && amount % 100 != 11
     return one
   elsif amount % 10 >= 2 && amount % 10 <= 4 && amount % 100 > 14
